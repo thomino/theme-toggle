89076df68265ac05166fb3aa5011d14a44bd9e20
Tomas Zeman <the.zeman@gmail.com>
2025-06-05 16:37:02 +0800

    Working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./App.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
-
+  content: [
+    './App.{js,ts,tsx}', 
+    './components/**/*.{js,ts,tsx}',
+    './providers/**/*.{js,ts,tsx}',
+    './utils/**/*.{js,ts,tsx}',
+  ],
+  darkMode: 'class',
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        background: "var(--color-background)",
+        text: "var(--color-text)",
+      },
+    },
   },
   plugins: [],
 };
